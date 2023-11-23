92ea51fd6e257772e9e47f36afd4261174434bd2
ARJU <arjunmk006@gmail.com>
2023-11-23 22:06:54 +0530

    Changed color theme, adjusted window size and reworked the GUI

--- calculator.py
+++ calculator.py
@@ -2,11 +2,13 @@ from tkinter import *
 import math
 import time
 root = Tk()
-# root.geometry("500x500")
+root.geometry("395x340")
 root.title("Calculator")
+root.resizable(False,False)
 
-e=Entry(root,width = 35, border = 5,font = 30)                      #textbox
-e.grid(row= 0,column= 0,columnspan= 4,ipady= 10)
+
+e=Entry(root,width = 35, border = 1,font = ("default",15),background="#565656",foreground="white")                    #textbox
+e.grid(row= 0,column= 0,columnspan= 4,ipady= 20)
 
 
 def buttonClick(number):                                            #onlcick event
@@ -71,23 +73,23 @@ def calculate():                                                     #function s
         
 
 #gui design
-
-button_one = Button(root,text="1",padx=30,pady=10,command= lambda: buttonClick(1)).grid(row=1,column=0)
-button_two = Button(root,text="2",padx=30,pady=10,command= lambda: buttonClick(2)).grid(row=1,column=1)
-button_three = Button(root,text="3",padx=30,pady=10,command= lambda: buttonClick(3)).grid(row=1,column=2)
-button_four = Button(root,text="4",padx=30,pady=10,command= lambda: buttonClick(4)).grid(row=2,column=0)
-button_five = Button(root,text="5",padx=30,pady=10,command= lambda: buttonClick(5)).grid(row=2,column=1)
-button_six = Button(root,text="6",padx=30,pady=10,command= lambda: buttonClick(6)).grid(row=2,column=2)
-button_seven = Button(root,text="7",padx=30,pady=10,command= lambda: buttonClick(7)).grid(row=3,column=0)
-button_eight = Button(root,text="8",padx=30,pady=10,command= lambda: buttonClick(8)).grid(row=3,column=1)
-button_nine = Button(root,text="9",padx=30,pady=10,command= lambda: buttonClick(9)).grid(row=3,column=2)
-button_zero = Button(root,text="0",padx=30,pady=10,command= lambda: buttonClick(0)).grid(row=4,column=0)
-button_clear = Button(root,text="C",padx=30,pady=10,command= delete).grid(row=4,column=1)
-button_equal = Button(root,text="=",padx=30,pady=10,command= calculate).grid(row=4,column=2)
-button_div = Button(root,text="/",padx=30,pady=10,command= div).grid(row=1,column=3)
-button_mul = Button(root,text="*",padx=30,pady=10,command= mul).grid(row=2,column=3)
-button_sub = Button(root,text="-",padx=30,pady=10,command= sub).grid(row=3,column=3)
-button_add = Button(root,text="+",padx=30,pady=10,command= add).grid(row=4,column=3)
+root.grid()
+button_one = Button(root,text="1",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(1)).grid(row=1,column=0)
+button_two = Button(root,text="2",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(2)).grid(row=1,column=1)
+button_three = Button(root,text="3",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(3)).grid(row=1,column=2)
+button_four = Button(root,text="4",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(4)).grid(row=2,column=0)
+button_five = Button(root,text="5",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(5)).grid(row=2,column=1)
+button_six = Button(root,text="6",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(6)).grid(row=2,column=2)
+button_seven = Button(root,text="7",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(7)).grid(row=3,column=0)
+button_eight = Button(root,text="8",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(8)).grid(row=3,column=1)
+button_nine = Button(root,text="9",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(9)).grid(row=3,column=2)
+button_zero = Button(root,text="0",font=("default",12),background="#565656",foreground="white",border=0.5,padx=40,pady=20,command= lambda: buttonClick(0)).grid(row=4,column=0)
+button_clear = Button(root,text="c",font=("default",12),background="#E0974C",foreground="white",border=0.5,padx=40,pady=20,command= delete).grid(row=4,column=1)
+button_equal = Button(root,text="=",font=("default",12),background="#3A3A3A",foreground="white",border=0.5,padx=40,pady=20,command= calculate).grid(row=4,column=2)
+button_div = Button(root,text="/",font=("default",13),background="#3A3A3A",foreground="white",border=0.5,padx=41,pady=20,command= div).grid(row=1,column=3)
+button_mul = Button(root,text="*",font=("default",13),background="#3A3A3A",foreground="white",border=0.5,padx=40,pady=20,command= mul).grid(row=2,column=3)
+button_sub = Button(root,text="-",font=("default",13),background="#3A3A3A",foreground="white",border=0.5,padx=40,pady=20,command= sub).grid(row=3,column=3)
+button_add = Button(root,text="+",font=("default",12),background="#3A3A3A",foreground="white",border=0.5,padx=39,pady=20,command= add).grid(row=4,column=3)
 
 
 root.mainloop()
